ab024bdc26ae771e7a1de535b805a7eef49c9e48
manasabyndoor <manasabyndoor@gmail.com>
2018-08-03 23:17:10 +0530

    BasicInheritance Vehicle problem aug-3

---
+++ src/First.java
@@ -0,0 +1,13 @@
+//Parent
+public class First {
+
+	void First() {
+		int i;
+	}
+	//method to display
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("First");
+	}
+
+}
---
+++ src/Seconds.java
@@ -0,0 +1,9 @@
+
+public class Seconds extends First {
+	// method for displaying
+	public void display() {
+		super.display();
+		System.out.println("Second");
+	}
+
+}
